cc49f448b1e27b7ff9515a321b4385c795e02f43
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2019-08-06 18:43:24 +0300

    Add WellDataset class

--- well_logs/core/__init__.py
+++ well_logs/core/__init__.py
@@ -1,6 +1,7 @@
-"""Core Well Logs objects."""
+"""Core classes of the package."""
 
 from .well import Well
 from .well_logs_batch import WellLogsBatch
 from .well_logs_dataset import WellLogsDataset
 from .well_batch import WellBatch
+from .well_dataset import WellDataset
