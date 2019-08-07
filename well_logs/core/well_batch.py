6d20124d59df7aa3b603151b4b97f70ff1e7ba4d
alexeykozhevin <a.kozhevin@analysiscenter.org>
2019-08-07 16:35:59 +0300

    Add docstrings to well_batch

--- well_logs/core/well_batch.py
+++ well_logs/core/well_batch.py
@@ -3,13 +3,18 @@
 from abc import ABCMeta
 from functools import wraps
 
+import numpy as np
+
 from ..batchflow import FilesIndex, Batch, action, inbatch_parallel
 from .well import Well
 from .abstract_classes import AbstractWell
 from .utils import to_list
 
+TARGET = dict() # inbatch_parallel target depending on action name
 
 class WellDelegatingMeta(ABCMeta):
+    """ Metaclass to delegate abstract methods from `WellBatch` to `Well` objects
+    in `wells` component. """
     def __new__(mcls, name, bases, namespace):
         abstract_methods = [base.__abstractmethods__ for base in bases if hasattr(base, "__abstractmethods__")]
         abstract_methods = frozenset().union(*abstract_methods)
@@ -25,16 +30,41 @@ class WellDelegatingMeta(ABCMeta):
             pos = self.get_pos(None, "wells", index)
             func = getattr(Well, name)
             self.wells[pos] = func(self.wells[pos], *args, **kwargs)
-        # TODO: choose inbatch_parallel target depending on action name
-        return action()(inbatch_parallel(init="indices", target="threads")(delegator))
+        target = TARGET.get(name, 'threads')
+        return action()(inbatch_parallel(init="indices", target=target)(delegator))
 
 
 class WellBatch(Batch, AbstractWell, metaclass=WellDelegatingMeta):
+    """A batch class for well data storing and processing.  Batch class inherits
+    all abstract methods from `Well` class and realize some extra functionality.
+    To execute method for each well in a batch you should add that method into pipeline.
+
+    Parameters
+    ----------
+    index : DatasetIndex
+        Unique identifiers of wells in the batch.
+    preloaded : tuple, optional
+        Data to put in the batch if given. Defaults to ``None``.
+
+    Attributes
+    ----------
+    index : DatasetIndex
+        Unique identifiers of wells in the batch.
+    wells : 1-D ndarray
+        An array of `Well` instances.
+
+    Note
+    ----
+    Some batch methods take ``index`` as their first argument after ``self``.
+    You should not specify it in your code since it will be implicitly passed
+    by ``inbatch_parallel`` decorator.
+    """
+
     components = ("wells",)
 
     def __init__(self, index, preloaded=None, **kwargs):
         super().__init__(index, preloaded, **kwargs)
-        self.wells = [None] * len(self.index)
+        self.wells = np.array([None] * len(self.index))
         self._init_wells(**kwargs)
 
     @inbatch_parallel(init="indices", target="threads")
@@ -52,9 +82,27 @@ class WellBatch(Batch, AbstractWell, metaclass=WellDelegatingMeta):
 
     @action
     def get_crops(self, src, dst):
+        """ Get some attributes from well and put them into batch variables.
+
+        Parameters
+        ----------
+        src : str or iterable
+            attributes of wells to load into batch
+        dst : str or iterable
+            batch variables to save well attributes. Must be of the same length as 'src'.
+
+        Returns
+        -------
+        batch : WellLogsBatch
+            Batch with loaded components. Changes batch data inplace.
+        """
         src = to_list(src)
         dst = to_list(dst)
+        if len(src) != len(dst):
+            raise ValueError(
+                "'src' and 'dst' must be of the same length but {} and {} were given".format(len(src), len(dst))
+            )
         for attr_from, attr_to in zip(src, dst):
-            crops = [getattr(segment, attr_from) for well in self.wells for segment in well.iter_level()]
-            setattr(self, attr_to, crops)
+            crops = [[getattr(segment, attr_from) for segment in well.iter_level()] for well in self.wells]
+            setattr(self, attr_to, np.array(crops))
         return self
