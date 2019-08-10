f04c4d15a29dad2d7c5037c29df789d8495b0e48
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2019-08-10 19:58:59 +0300

    Update to_list docstring

--- well_logs/core/utils.py
+++ well_logs/core/utils.py
@@ -29,6 +29,6 @@ def for_each_component(method):
 def to_list(obj):
     """Cast an object to a list. Almost identical to `list(obj)` for 1-D
     objects, except for `str`, which won't be split into separate letters but
-    transformed into a list of one element.
+    transformed into a list of a single element.
     """
     return np.array(obj).ravel().tolist()
