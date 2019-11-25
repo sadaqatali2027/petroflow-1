ea22935160c6724c24dad8b329242c60c27e0d3a
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2019-11-25 17:43:11 +0300

    Update batchflow

--- petroflow/src/well_batch.py
+++ petroflow/src/well_batch.py
@@ -75,8 +75,6 @@ class WellBatch(Batch, AbstractWell, metaclass=WellDelegatingMeta):
         if preloaded is None:
             self.wells = np.array([None] * len(self.index))
             self._init_wells(**kwargs)
-        else:  # Remove when batch.as_dataset is fixed
-            self.wells = np.array([deepcopy(preloaded[0][k]) for k in index.indices] + [None])[:-1]
 
     @inbatch_parallel(init="indices", target="threads")
     def _init_wells(self, index, src=None, **kwargs):
