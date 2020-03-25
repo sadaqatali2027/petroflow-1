e72d9818d663f9ed5dcf3d9d258787bef4156c68
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2020-03-25 18:59:09 +0300

    Delete legacy code

--- petroflow/src/__init__.py
+++ petroflow/src/__init__.py
@@ -1,9 +1,7 @@
 """Core classes of the package."""
 
 from .well import Well
-from .well_logs_batch import WellLogsBatch
-from .well_logs_dataset import WellLogsDataset
-from .core_images import CoreBatch, CoreIndex
 from .well_batch import WellBatch
 from .well_dataset import WellDataset
 from .named_expr import WS
+from .core_images import CoreBatch, CoreIndex
